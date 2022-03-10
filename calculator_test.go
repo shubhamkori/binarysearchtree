89f4c71d17a287c85dc62bb0f921a8e0c30246ca
shubham <shubhamkori310@gmail.com>
2022-03-10 16:36:40 +0530

    feat: :test_tube: 5 and 2 addition no.

--- calculator_test.go
+++ calculator_test.go
@@ -5,4 +5,17 @@ import "testing"
 func Test_add_5_2(t *testing.T) {
 
 	t.Logf("Hello")
+	// define variables for test / Preparation
+	a := 5
+	b := 2
+	wanted := 7
+
+	// call function to test
+	got := Add(a, b)
+
+	// verify result
+	if got != wanted {
+		t.Errorf("Add(%d,%d) = %d, wanted %d", a, b, got, wanted)
+	}
+
 }
