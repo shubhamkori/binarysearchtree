2ed0b3345dee0e74a9b5745731cb389a1b532b3e
shubham <shubhamkori310@gmail.com>
2022-03-11 15:39:59 +0530

    0,0 adding

--- calculator_test.go
+++ calculator_test.go
@@ -36,3 +36,20 @@ func Test_add_2_0(t *testing.T) {
 	}
 
 }
+func Test_add_0_0(t *testing.T) {
+
+	t.Logf("Hello")
+	// define variables for test / Preparation
+	a := 0
+	b := 0
+	wanted := 0
+
+	// call function to test
+	got := Add(a, b)
+
+	// verify result
+	if got != wanted {
+		t.Errorf("Add(%d,%d) = %d, wanted %d", a, b, got, wanted)
+	}
+
+}
